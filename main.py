1526fc665338acd9fc96e9946ffe844c74f84edc
AJaxx <62261949+AJaxx86@users.noreply.github.com>
2026-04-08 01:40:13 +0100

    settings redone

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.widgets import Button, Input, Label
 
-from tui.settings import SettingsPage
+from tui.settings import Settings
 from tui.setup import SetupPage
 
 
@@ -17,34 +17,26 @@ class TopBar(Horizontal):
 		title.styles.text_align = "center"
 		title.styles.text_style = "bold"
 		yield title
+		yield Button("<-", id="back")
 
 
 class MediaRenamer(App):
 	load_dotenv()
 	openrouter_key, ollama_port = os.getenv("OPENROUTER_KEY"), os.getenv("OLLAMA_PORT")
-	working_dir: str = ""
 
 	def compose(self) -> ComposeResult:
 		with Vertical():
 			yield TopBar()
 			with Horizontal():
-				yield Input(placeholder="Enter scan directory")
-			with Horizontal():
-				yield Button("Confirm", id="confirm")
+				yield Settings()
 
-	def on_button_pressed(self, event: Button.Pressed) -> None:
-		if event.button.id == "confirm":
-			input_widget = self.query_one(Input)
-			self.working_dir = input_widget.value
-
-			if not os.path.isdir(self.working_dir):
-				self.notify(f"'{self.working_dir}' isn't a valid path.", severity="error")
-				return
-			self.app.push_screen(SettingsPage(scan_dir=self.working_dir))
-	
 	def on_mount(self) -> None:
 		if not self.openrouter_key and not self.ollama_port:
 			self.push_screen(SetupPage())
+	
+	def on_button_pressed(self, event: Button.Pressed) -> None:
+		if event.button.id == "back":
+			self.app.push_screen(SetupPage())
 
 
 if __name__ == "__main__":
--- tui/settings.py
+++ tui/settings.py
@@ -1,33 +1,79 @@
 from textual.app import ComposeResult
-from textual.containers import Horizontal, Vertical
+from textual.containers import Horizontal, Vertical, VerticalScroll
 from textual.screen import Screen
-from textual.widgets import Button, Collapsible, Label
+from textual.widgets import Button, Collapsible, Label, Input, TextArea
 
 
-class TopBar(Horizontal):
+class ScanDir(Vertical):
 	def compose(self) -> ComposeResult:
-		self.styles.height = "auto"
-		title = Label("Settings")
-		title.styles.width = "100%"
-		title.styles.text_align = "center"
-		title.styles.text_style = "bold"
-		yield title
+		yield Label("Scan Directory")
+		with Horizontal():
+			yield Input("e.g. C:/Users/Steve/Pictures")
+			yield Button("Scan", id="find_files")
 
 
-class SettingsPage(Screen):
-	working_dir: str = ""
+class Include(Vertical):
+	allow_images: bool
+	allow_videos: bool
 
-	def __init__(self, scan_dir: str):
+	def __init__(self, images: bool, videos: bool) -> None:
 		super().__init__()
-		self.working_dir = scan_dir
-		self.log(f"working dir set: {self.working_dir}")
+		self.allow_images = images
+		self.allow_videos = videos
 
 	def compose(self) -> ComposeResult:
-		with Vertical():
-			yield TopBar()
-			yield Label(f"Scan directory set to '{self.working_dir}'.")
-			yield Button("Back", id="back")
+		yield Label("Include")
+		with Horizontal():
+			yield Button(f"[{"X" if self.allow_images else " "}] Images", id="include_images")
+			yield Button(f"[{"X" if self.allow_videos else " "}] Videos", id="include_videos")
+
+
+class ModelConfig(Vertical):
+	def compose(self) -> ComposeResult:
+		yield Label("Extra Information")
+		yield TextArea(placeholder="If you'd like, add extra context to get more specific names.", id="user_prompt")
+		yield Label("Model")
+		with Horizontal():
+			model_type = Button("Cloud", id="model_type")
+			model_type.styles.color = "green"
+			yield model_type
+
+
+class Settings(Vertical):
+	working_dir: str = ""
+	images: list[str] = []
+	videos: list[str] = []
+
+	include_images: bool = False
+	include_videos: bool = True
+	
+	cloud_model: bool = True
+
+	def compose(self) -> ComposeResult:
+		yield ScanDir()
+		yield Include(self.include_images, self.include_videos)
+		yield ModelConfig()
+
+		start_button = Button("Start Analyses", id="start_analyses")
+		yield start_button
 
 	def on_button_pressed(self, event: Button.Pressed) -> None:
-		if event.button.id == "back":
-			self.app.pop_screen()
+		if event.button.id == "find_files":
+			pass
+
+		elif event.button.id == "include_images":
+			self.include_images = not self.include_images
+			event.button.label = f"[{'X' if self.include_images else ' '}] Images"
+
+		elif event.button.id == "include_videos":
+			self.include_videos = not self.include_videos
+			event.button.label = f"[{'X' if self.include_videos else ' '}] Videos"
+		
+		elif event.button.id == "model_type":
+			self.cloud_model = not self.cloud_model
+			if self.cloud_model == True:
+				event.button.label = "[Cloud]"
+				event.button.styles.color = "blue"
+			else:
+				event.button.label = "[Local]"
+				event.button.styles.color = "orange"
